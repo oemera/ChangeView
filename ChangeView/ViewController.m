856557cd3a4764009780399fb1fc9912390057da
Ömer Avci <oemer.a@me.com>
2012-09-15 20:30:51 +0200

    initial commit

--- ChangeView/ViewController.m
+++ ChangeView/ViewController.m
@@ -10,20 +10,42 @@
 
 @interface ViewController ()
 
+@property (nonatomic, assign) BOOL isShowingBanner;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)changeView:(id)sender {
+    if (self.isShowingBanner) {
+        UIView *existingView = self.view.subviews[0];
+        
+        CGRect existingViewFrame = existingView.frame;
+        existingViewFrame.size.height += 50;
+        existingView.frame = existingViewFrame;
+        
+        self.view = existingView;
+        self.isShowingBanner = NO;
+    } else {
+        UIView *existingView = self.view;
+        [existingView setBackgroundColor:[UIColor greenColor]];
+        
+        UIView *newView = [[UIView alloc] initWithFrame:existingView.frame];
+        [newView setBackgroundColor:[UIColor redColor]];
+        
+        UIView *bannerView = [[UIView alloc] initWithFrame:CGRectMake(0, (self.view.frame.size.height - 50), 320, 50)];
+        [bannerView setBackgroundColor:[UIColor blueColor]];
+        
+        CGRect existingViewFrame = existingView.frame;
+        existingViewFrame.size.height -= 50;
+        existingView.frame = existingViewFrame;
+        
+        [newView addSubview:existingView];
+        [newView addSubview:bannerView];
+        
+        self.view = newView;
+        self.isShowingBanner = YES;
+    }
 }
 
 @end
